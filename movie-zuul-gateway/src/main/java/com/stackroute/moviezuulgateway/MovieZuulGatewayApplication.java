87bf455f7fa584c8a8c8c66759d51c3ddbee73c3
j3rwin <jerwin.j@cgi.com>
2019-10-12 09:17:56 +0530

    dockerize
    Signed-off-by: j3rwin <jerwin.j@cgi.com>

--- movie-zuul-gateway/src/main/java/com/stackroute/moviezuulgateway/MovieZuulGatewayApplication.java
+++ movie-zuul-gateway/src/main/java/com/stackroute/moviezuulgateway/MovieZuulGatewayApplication.java
@@ -8,9 +8,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
 public class MovieZuulGatewayApplication {
 
 	public static void main(String[] args) {
